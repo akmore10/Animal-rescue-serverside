9875b3c2b32d7bc8fe56623655e6922310f584e4
akmore10 <akmore90@gmail.com>
2022-06-08 14:00:50 +0530

    Animal Rescue serverside

---
+++ routes/animalReport.js
@@ -0,0 +1,115 @@
+const router = require('express').Router();
+let Animal = require('../models/AnimalReport.model');
+
+
+const verifyToken = (req,res,next)=>{
+    const token = req.headers['x-access-token'];
+    if(!token){
+        res.json("Something went wrong");
+    }
+    else{
+        jwt.verify(token,"sdfajslbasnfdaslkzxcxmasfnecxvasnmvnoierasdn",(err,decoded)=>{
+            if (err){
+                res.json({auth:false,message:"Something is wrong"});
+            }else{
+                req.userId = decoded.id;
+                next();
+            }
+        })
+    }
+    
+}
+
+router.route('/api/create').post((req,res)=>{
+    const reportedId = req.body.recordId;
+    const name = req.body.name;
+    const description = req.body.description;
+    const city = req.body.city;
+    const state = req.body.state;
+    const address = req.body.address;
+    const pincode = req.body.pincode;
+    const photo = req.body.photo;
+    const takenBy = 'F'
+    const acceptedId = 'None'
+    console.log(req.body);
+
+    const newReport = new Animal({
+        reportedId,
+        name,
+        description,
+        city,
+        state,
+        address,
+        pincode,
+        photo,
+        takenBy,
+        acceptedId
+    });
+    newReport.save()
+    .then(()=> res.json('Report added!'))
+    .catch((err)=> res.status(400).json(err));
+});
+
+router.route('/api/view').get((req,res)=>{
+
+    Animal.find()
+    .then(report=>{
+        res.json(report);
+    })
+    .catch(err=>res.status(400).json('Error: '+err));
+});
+
+
+router.route('/api/showall').post((req,res)=>{
+
+    Animal.find()
+    .then(disaster=>{
+        res.json(disaster);
+    })
+    .catch(err=>res.status(400).json('Error: '+err));
+});
+
+
+
+router.route('/api/delete').post(verifyToken,(req,res)=>{
+    Disaster.remove({ _id: req.body.id }, function(err) {  
+        if(err){  
+            res.send(err);  
+        }  
+        else{    
+               res.send({data:"Record has been Deleted..!!"});             
+           }  
+    });  
+})
+
+router.route('/api/update').post(verifyToken,(req,res)=>{
+    Disaster.findById(req.body._id)
+    .then(disaster=>{
+        disaster.disName = req.body.disName;
+        disaster.disState = req.body.disState;
+        disaster.disCity = req.body.disCity;
+        disaster.disAddress = req.body.disAddress;
+        disaster.disPhoto = req.body.disPhoto;
+
+        disaster.save()
+        .then(()=> res.json('Disaster updated!'))
+        .catch((err)=> res.status(400).json(err));
+    })
+    .catch(err=>res.status(400).json('Error: '+err));
+})
+
+
+router.route('/api/accept').post((req,res)=>{
+    Animal.findById({_id:req.body.id})
+    .then(animal=>{
+        console.log(animal)
+        animal.takenBy = 'T';
+        animal.acceptedId = req.body.acceptedId;
+        animal.save()
+        .then(()=> res.json('Animal updated!'))
+        .catch((err)=> res.status(400).json(err));
+    })
+    .catch(err=>res.status(400).json('Error: '+err));
+})
+
+module.exports = router
---
+++ routes/randomUser.js
@@ -0,0 +1,120 @@
+const router = require('express').Router();
+let User = require('../models/RandomUser.model');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+router.route('/api/signup').post((req,res)=>{
+        const name = req.body.name;
+		const phone = req.body.phone;
+		const city = req.body.city;
+        const address = req.body.address;
+		const state = req.body.state;
+		const pincode = req.body.pincode;
+		const email = req.body.email;
+        const verified = 'F';
+        const inAppCurrency = 0
+        const verificationDocument = ''
+		const password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync());
+        const creditScore = 0;
+		const newUser = new User({
+            name,
+            phone,
+            city,
+            state,
+            address,
+            pincode,
+            email,
+            password,
+            verificationDocument,
+            inAppCurrency,
+            verified,
+            creditScore
+        });
+        newUser.save()
+        .then(()=>res.json({success:true}))
+        .catch((err)=> {
+            }
+        )
+
+})
+
+router.route('/api/signin').post((req,res)=>{
+    const email = req.body.email;
+    
+    User.find({email:email})
+    .then((users)=>{
+        if (users.length > 0){
+            let validatePassword = bcrypt.compareSync(req.body.password, users[0].password);
+            console.log(validatePassword);
+            if (validatePassword) {
+                const id = users[0]._id;
+                const token = jwt.sign({id},"sdfajslbasnfdaslkzxcxmasfnecxvasnmvnoierasdn",{
+                    expiresIn: "1h",
+
+                });
+
+                res.json({auth:true,token:token,id_user:users[0]._id,name:users[0].name});
+            }
+            else{
+                res.json({auth:false});
+            }
+        }else{
+            res.json("Something went wrong");
+        }
+    })
+    .catch(err=>res.status(400).json('Error: '+err));
+
+});
+
+
+
+
+
+router.route('/api/forgotpassword').post((req,res)=>{
+    User.find({email:req.body.email})
+    .then(users => {
+        if (users.length > 0){
+            res.send({user:true});
+        }
+        else{
+            res.send({user:false});
+        }
+    })
+})
+
+router.route('/api/changePassword').post((req,res)=>{
+    User.findOneAndUpdate({ngoEmail:req.body.email},{$set:{ngoPassword:bcrypt.hashSync(req.body.password, bcrypt.genSaltSync())}})
+    .then(res=>{
+        console.log("Password changed");
+    })
+    res.json({password:true});  
+})
+
+router.route('/api/document').post((req,res)=>{
+    User.findOneAndUpdate({_id:req.body.id_user},{$set:{verificationDocument:req.body.document}})
+    .then(res=>{
+        res.json({document:true})
+    })
+    
+})
+
+
+router.route('/api/updateCoins').post((req,res)=>{
+
+    User.find({_id:req.body.id_user})
+    .then(result=>{
+        
+        const coins = result[0].inAppCurrency + Number(req.body.coins);
+        User.findOneAndUpdate({_id:req.body.id_user},{$set:{inAppCurrency:coins}})
+        .then(result=>{
+            res.json({success:true});      
+        })
+    })    
+})
+
+
+router.route('/api/show').post((req,res)=>{
+    User.find({_id:req.body.id_user})
+    .then(result=>{res.json(result)})
+})
+
+module.exports = router;
